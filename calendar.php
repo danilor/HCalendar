604087a8450a162dbf1f9aeda2c54d97216163ed
Danilo Ramirez <danilo@globalex.cr>
2016-06-15 10:08:00 -0600

    Calendar print

--- calendar.php
+++ calendar.php
@@ -4,12 +4,18 @@
 <html>
     <?php include("blocks/head.php"); ?>
     <body>
-            <h1>Calendar Display</h1>
-            <h4>Date since: <?php echo $dated->format("m/d/Y") ?> | Days: <?php echo $days ?> | Country Code: <?php echo $cc ?></h4>
+            <div class="wrapper">
+                <div class="col-xs-12">
+                    <h1>Calendar Display</h1>
+                    <h4>Date since: <?php echo $dated->format("m/d/Y") ?> | Days: <?php echo $days ?> | Country Code: <?php echo $cc ?></h4>
 
-            <?php
-                echo get_calendar_month_year(03,2016); // This function should be the one
-            ?>
+                    <br />
+                    <?php
+                        echo get_calendar_month_year($dated->format("m"),$dated->format("Y"),true); // This function should be the one
+                    ?>
+                </div>
+            </div>
+
 
     </body>
     <?php include("blocks/scripts.php"); ?>
